45d38b596d5b6821721d0515cba8c2ee308a0870
= <=>
2025-05-04 15:42:15 -0300

    feat: criar modelo Cor

---
+++ core/models/cor.py
@@ -0,0 +1,8 @@
+# core/models/cor.py
+from django.db import models
+
+class Cor(models.Model):
+    nome = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"{self.nome} ({self.id})"
